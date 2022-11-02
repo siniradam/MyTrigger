a1e130d0dd343680beb5878bc56977ad5487c96f
Koray K <3178435+siniradam@users.noreply.github.com>
2022-11-01 20:01:12 -0700

    Initial Commit

---
+++ My Trigger Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  My Trigger Watch App
+//
+//  Created by Koray KIRCAOGLU on 11/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Trigger Watch App/My_TriggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_TriggerApp.swift
+//  My Trigger Watch App
+//
+//  Created by Koray KIRCAOGLU on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct My_Trigger_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
