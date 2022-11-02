ab538c6d7639e6128206bd0e28ac0405597c2a4e
Koray K <3178435+siniradam@users.noreply.github.com>
2022-11-02 00:26:14 -0700

    App Structure

--- My Trigger Watch App/ContentView.swift
+++ My Trigger Watch App/ContentView.swift
@@ -7,13 +7,79 @@
 
 import SwiftUI
 
+var gotResult = false;
+
+func setTrigger(command:String) async{
+    let url = URL(string: "http://192.168.4.67:1337/")!
+    var request = URLRequest(url: url);
+    let body: [String: String] = ["command": command, "device": "watch"]
+ 
+    do{
+        let finalBody = try JSONSerialization.data(withJSONObject: body)
+        
+        request.httpMethod = "POST";
+        request.httpBody = finalBody
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept");
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            // try to read out a string array
+            if let result = json["result"] as? [String] {
+                print(result)
+            }
+        }
+        gotResult = true
+        await fetchData()
+    }catch{
+        print(error)
+    }
+
+}
+
+func fetchData() async {
+        
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Select Trigger")
+            Button("Sleep"){
+                Task {
+                    await setTrigger(command: "sleep")
+                }
+            }
+            .foregroundColor(Color.black)
+            .background(Color.init(UIColor(red:235/255, green:180/255, blue:52/255, alpha: 1)))
+            .cornerRadius(25)
+            
+            
+            HStack{
+                Button(action:{
+                    Task {
+                        await setTrigger(command: "fav")
+                    }
+                },label: {
+                    Text("FAV")
+                })
+                .background(Color.blue)
+                .contentShape(Circle())
+                .cornerRadius(30)
+                
+                Button(action:{
+                    Task {
+                        await setTrigger(command: "turnoff")
+                    }
+                },label: {
+                    Text("Off")
+                })
+                .background(Color.red)
+                .contentShape(Circle())
+                .cornerRadius(30)
+                .frame(width: 60)
+            }
+            
         }
         .padding()
     }
